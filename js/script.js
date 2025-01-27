d18e1e24517a962e9260168ccb97d4d01c939d55
michellebirkkjaer <michelle.birkkjaer@hotmail.dk>
2025-01-27 09:41:26 +0100

    opg kan ikke løses da const ikke kan bruges to gange

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const firstMessage = `I'm learning to code with JavaScript`;
+
+console.log(`${firstMessage}`);
+
+const firstMessage = `I want to become a good JavaScript programmer`;
+
+console.log(`${firstMessage}`);
+
+
+//man kan ikke lave to const
+
+// let kan man senere ændre - så skal man bare ikke skrive let foran feks "firstMessage". Man skriver bare firstMessage = den nye formulering.
